2a2a9b9d87342a908dd6acaffe36c457cb7ecc1d
Mirzo-Golib <mirzogolib_96@mail.ru>
2017-10-18 19:03:59 +0500

    myCommit

--- app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
+++ app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
@@ -1,13 +1,18 @@
 package uz.iutlab.safarkompasi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_page);
+        TextView myTextView = (TextView) findViewById(R.id.name);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Avenir-Book.ttf");
+        myTextView.setTypeface(typeface);
     }
 }
