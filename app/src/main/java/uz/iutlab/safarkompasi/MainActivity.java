09688e69f261104c5aadbc0a0b90ac401162cde8
nameclub <mr.abdukhamidov@mail.ru>
2017-10-19 05:43:29 -0700

    \style changed

--- app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
+++ app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,7 +44,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-      finish();
+//        super.onBackPressed();
+
+        if(backButtonCount >= 1)
+        {
+            finish();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
+
     }
 
 
