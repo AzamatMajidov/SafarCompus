72637e2fb4f0b2f840a1720151498a043be29d09
Azamat Majidov <a.majidov@student.inha.uz>
2017-10-19 17:29:33 +0500

    some news

--- app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
+++ app/src/main/java/uz/iutlab/safarkompasi/MainActivity.java
@@ -1,27 +1,55 @@
 package uz.iutlab.safarkompasi;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
-    int backButtonCount=0;
+
+    @BindView(R.id.name)
+    TextView appName;
+    @BindView(R.id.news_btn)
+    Button news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_page);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        TextView myTextView = (TextView) findViewById(R.id.name);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Avenir-Book.ttf");
-        myTextView.setTypeface(typeface);
+
+        setTypeFace();
+
+
+
+
     }
 
+    private void setTypeFace() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Avenir-Book.ttf");
+        appName.setTypeface(typeface);
+    }
 
 
     @Override
     public void onBackPressed() {
       finish();
+    }
+
+
+    @OnClick(R.id.news_btn)
+    public void onNewsClick(View view){
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
     }
 }
